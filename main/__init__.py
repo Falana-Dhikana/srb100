0ea7d3913a348d974fbb4e537c325cd27060ed0e
Falana <125483872+Falana-Dhikana@users.noreply.github.com>
2024-07-07 03:42:19 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -15,12 +15,12 @@ logging.getLogger("telethon").setLevel(logging.WARNING)
 
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None)
+API_ID = config("API_ID", default=7338003139, cast=int)
+API_HASH = config("API_HASH", default="AAGiAYUP7dIhX8s383c6jA4roEs0iBA9qdc")
+BOT_TOKEN = config("BOT_TOKEN", default=7338003139:AAGiAYUP7dIhX8s383c6jA4roEs0iBA9qdc)
+SESSION = config("SESSION", default="BQG17rgAFWtWqO97IIkez_OSad6PDQUXTmSmcfUEevV4O38hPIIuEF52XgYvSyjGGECpcKDuf7mRK9ivAzLcJJPenBxW3StOX4LOxWePqc0rt1UA1MTVeTVgeaINIjJOxAIbXeL95e3i7TOnoXTx8V4SDw2EH04OP085METsKg7A3aIIFfyAEJ1B1IXo9unmLhgIu1Yn9XbDqZzDSNwWd32ba4u7EMrU1IVd8G98HdTwVFY1BbYVYCtSaFF6FSHBtkg1CAmAIki8aKz8PB-e8ELG4gDEdhzQ8R_RphC7pPuvVMX0zJPbu2GQkZWbtnqMViHr5Vdc841ZqZP77C2b8jzIjmZz8QAAAAErSvw7AA")
+FORCESUB = config("FORCESUB", default="Genetry")
+AUTH = config("AUTH", default=6163696719)
 SUDO_USERS = []
 if len(AUTH) != 0:
     SUDO_USERS = {int(AUTH.strip()) for AUTH in AUTH.split()}
